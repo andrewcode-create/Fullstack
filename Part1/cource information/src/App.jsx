9206db95193555169764f1a256e6a1ad8a10f39b
Andrew Haas <66093025+andrewcode-create@users.noreply.github.com>
2023-12-07 10:58:39 -0600

    work on part1

--- Part1/cource information/src/App.jsx
+++ Part1/cource information/src/App.jsx
@@ -1,34 +1,41 @@
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
-  const partarr = [part1, part2, part3];
-  const exercisesarr = [exercises1, exercises2, exercises3];
-
   console.clear();
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
       <Header course={course} />
-      <Content partarr={partarr} exercisesarr={exercisesarr} />
-      <Total exercisesarr={exercisesarr} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
 export default App;
 
 const Header = (props) => {
-  return <h1>{props.course}</h1>;
+  return <h1>{props.course.name}</h1>;
 };
 
 const Part = (props) => {
   return (
     <p>
-      {props.part} {props.exercise}
+      {props.part.name} {props.part.exercises}
     </p>
   );
 };
@@ -36,16 +43,19 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.partarr[0]} exercise={props.exercisesarr[0]} />
-      <Part part={props.partarr[1]} exercise={props.exercisesarr[1]} />
-      <Part part={props.partarr[2]} exercise={props.exercisesarr[2]} />
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
     </div>
   );
 };
+
 const Total = (props) => {
   return (
     <p>
-      {props.exercisesarr[0] + props.exercisesarr[1] + props.exercisesarr[2]}
+      {props.parts[0].exercises +
+        props.parts[1].exercises +
+        props.parts[2].exercises}
     </p>
   );
 };
