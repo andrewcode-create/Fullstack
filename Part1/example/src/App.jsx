69b354c3725733cb6b63099dab6846aee1931c23
Andrew Haas <66093025+andrewcode-create@users.noreply.github.com>
2023-12-07 13:43:12 -0600

    example button

--- Part1/example/src/App.jsx
+++ Part1/example/src/App.jsx
@@ -3,9 +3,26 @@ import { useState } from "react";
 const App = () => {
   const [counter, setCounter] = useState(0);
 
-  setTimeout(() => setCounter(counter + 1), 1000);
+  const increaseByOne = () => setCounter(counter + 1);
+  const decreaseByOne = () => setCounter(counter - 1);
+  const setToZero = () => setCounter(0);
 
-  return <div>{counter}</div>;
+  return (
+    <div>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={setToZero} text="zero" />
+      <Button onClick={decreaseByOne} text="minus" />
+    </div>
+  );
+};
+
+const Display = (props) => {
+  return <div>{props.counter}</div>;
+};
+
+const Button = (props) => {
+  return <button onClick={props.onClick}>{props.text}</button>;
 };
 
 export default App;
