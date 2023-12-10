9973e0f7087611b2b33224e033acf0fed81ea20d
Andrew Haas <66093025+andrewcode-create@users.noreply.github.com>
2023-12-10 11:49:21 -0600

    working unicafe

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -1,63 +1,64 @@
 import { useState } from "react";
 
-const App = () => {
-  console.clear();
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
-  };
+const StatisticLine = ({ text, value, suffix }) => {
+  return (
+    <tr>
+      <td>{text} </td>
+      <td>
+        {value}
+        {suffix}
+      </td>
+    </tr>
+  );
+};
+
+const Statistics = ({ good, neutral, bad }) => {
+  let all = good + neutral + bad;
+  if (all === 0) {
+    return <p>No Feedback Given</p>;
+  }
 
   return (
-    <div>
-      <Header course={course} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
+    <>
+      <h1>statistics</h1>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={all} />
+          <StatisticLine text="average" value={(good - bad) / all} />
+          <StatisticLine
+            text="positive"
+            value={(good * 100) / all}
+            suffix=" %"
+          />
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+const Button = ({ text, onClick }) => {
+  return <button onClick={onClick}>{text}</button>;
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  //console.clear();
+  console.log("re-render");
+  //debugger;
+  return (
+    <>
+      <h1>give feedback</h1>
+      <Button onClick={() => setGood(good + 1)} text="Good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="bad" />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </>
   );
 };
 export default App;
-
-const Header = (props) => {
-  return <h1>{props.course.name}</h1>;
-};
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>
-  );
-};
-
-const Content = (props) => {
-  return (
-    <div>
-      <Part part={props.parts[0]} />
-      <Part part={props.parts[1]} />
-      <Part part={props.parts[2]} />
-    </div>
-  );
-};
-
-const Total = (props) => {
-  return (
-    <p>
-      {props.parts[0].exercises +
-        props.parts[1].exercises +
-        props.parts[2].exercises}
-    </p>
-  );
-};
