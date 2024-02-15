a822fd3c8ea51008ac6c5db3626b3454b6884a2c
Andrew Haas <66093025+andrewcode-create@users.noreply.github.com>
2024-02-15 12:18:52 -0600

    added getting notes from the server

--- Part2/part2-notes/src/main.jsx
+++ Part2/part2-notes/src/main.jsx
@@ -1,11 +1,4 @@
 import ReactDOM from "react-dom/client";
-import axios from "axios";
 import App from "./App";
 
-axios.get("http://localhost:3001/notes").then((response) => {
-  const notes = response.data;
-  console.log("Got notes from server");
-  ReactDOM.createRoot(document.getElementById("root")).render(
-    <App notes={notes} />
-  );
-});
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
