9fc346aa7f97a761725bb52230d27697f63db34c
Andrew Haas <66093025+andrewcode-create@users.noreply.github.com>
2024-03-03 16:16:37 -0600

    finished notes but not excersie 2.12

--- Part2/part2-notes/src/App.jsx
+++ Part2/part2-notes/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Note from "./components/Note";
+import noteService from "./services/notes";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
@@ -8,13 +9,36 @@ const App = () => {
   const [showAll, setShowAll] = useState(true);
 
   useEffect(() => {
-    axios.get("http://localhost:3001/notes").then((response) => {
-      setNotes(response.data);
+    noteService.getAll().then((initialnotes) => {
+      setNotes(initialnotes);
+      console.log(
+        `Got initial notes. ${initialnotes.map(
+          (note) =>
+            " [content: " +
+            note.content +
+            "; important: " +
+            note.important +
+            "]"
+        )}`
+      );
     });
   }, []);
 
   const toggleImportanceOf = (id) => {
-    console.log(`importance of ${id} needs to be toggled`);
+    const url = `http://localhost:3001/notes/${id}`;
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    noteService
+      .update(id, changedNote)
+      .then((returnedNote) => {
+        console.log(`toggle importance of note ${id}`);
+        setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
+      })
+      .catch((error) => {
+        alert(`the note '${note.content}' was already deleted from server`);
+        setNotes(notes.filter((n) => n.id !== id));
+      });
   };
 
   const addNote = (event) => {
@@ -24,8 +48,8 @@ const App = () => {
       important: Math.random() > 0.5,
     };
 
-    axios.post("http://localhost:3001/notes", noteObject).then((response) => {
-      setNotes(notes.concat(response.data));
+    noteService.create(noteObject).then((returnedNote) => {
+      setNotes(notes.concat(returnedNote));
       setNewNote("");
     });
   };
