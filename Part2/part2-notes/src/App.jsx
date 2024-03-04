dc658b1482dad82100c316c712229e9f6c1c780a
Andrew Haas <66093025+andrewcode-create@users.noreply.github.com>
2024-03-03 21:42:41 -0600

    added CSS to notes

--- Part2/part2-notes/src/App.jsx
+++ Part2/part2-notes/src/App.jsx
@@ -3,26 +3,51 @@ import axios from "axios";
 import Note from "./components/Note";
 import noteService from "./services/notes";
 
+const Footer = () => {
+  const footerStyle = {
+    color: "darkGreen",
+    fontStyle: "italic",
+    fontSize: 16,
+  };
+  return (
+    <div style={footerStyle}>
+      <br />
+      <>
+        Note app, Andrew Haas <br /> Created using a 2024 fullstack development
+        cource by the Department of Computer Science in the University of
+        Helsinki
+      </>
+    </div>
+  );
+};
+
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
     noteService.getAll().then((initialnotes) => {
       setNotes(initialnotes);
       console.log(
         `Got initial notes. ${initialnotes.map(
-          (note) =>
-            " [content: " +
-            note.content +
-            "; important: " +
-            note.important +
-            "]"
+          (note) => `[content: ${note.content}; important: ${note.important}]`
         )}`
       );
     });
+    setTimeout(() => {
+      setErrorMessage(null);
+    }, 1000);
   }, []);
+
+  const Notification = ({ message }) => {
+    if (message === null) {
+      return null;
+    }
+
+    return <div className="error">{message}</div>;
+  };
 
   const toggleImportanceOf = (id) => {
     const note = notes.find((n) => n.id === id);
@@ -35,7 +60,12 @@ const App = () => {
         setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
       })
       .catch((error) => {
-        alert(`the note '${note.content}' was already deleted from server`);
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        );
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
         setNotes(notes.filter((n) => n.id !== id));
       });
   };
@@ -62,6 +92,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? "important" : "all"}
@@ -80,6 +111,7 @@ const App = () => {
         <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
+      <Footer />
     </div>
   );
 };
