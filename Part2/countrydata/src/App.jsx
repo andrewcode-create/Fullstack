d6db1f75393dbca462159714aef933750de22369
Andrew Haas <66093025+andrewcode-create@users.noreply.github.com>
2024-03-04 08:50:59 -0600

    working countrydata search

--- Part2/countrydata/src/App.jsx
+++ Part2/countrydata/src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [searchCountry, setSearchCountry] = useState("");
+  const [allCountries, setAllCountries] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
+      .then((response) => {
+        setAllCountries(response.data);
+        console.log("got initial country data");
+      });
+  }, []);
+
+  const handleCountryChange = (event) => {
+    setSearchCountry(event.target.value);
+    console.log("change country");
+  };
+
+  const countryToPrint = () => {
+    const specific = allCountries
+      .filter((country) =>
+        country.name.common.toLowerCase().includes(searchCountry.toLowerCase())
+      )
+      .map((country) => country.name.common);
+    console.log(`Seearch has ${specific.length} matches`);
+    return specific;
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <p>Find counries </p>
+      <input value={searchCountry} onChange={handleCountryChange} />
+      <p>{countryToPrint()}</p>
+    </div>
+  );
+};
 
-export default App
+export default App;
