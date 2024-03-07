61e515d906a7765e34a1399ee2207da5e0f47cb0
Andrew Haas <66093025+andrewcode-create@users.noreply.github.com>
2024-03-07 09:26:04 -0600

    added rest api tests

--- Part3/index.js
+++ Part3/index.js
@@ -19,6 +19,33 @@ let notes = [
   },
 ];
 
+app.use(express.json());
+
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+
+  response.json(note);
+});
+
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
@@ -53,4 +80,5 @@ app.get("/api/notes/:id", (request, response) => {
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log(`URL: http://localhost:${PORT}/`);
 });
