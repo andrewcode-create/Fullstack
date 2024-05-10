f89e88c86fa1dea9a7c6d9845d3686ddb13caaab
Andrew Haas <ahaas@micds.org>
2024-05-10 17:42:09 +0000

    did 3.12

--- Part3/phonebook/backend/mongo.js
+++ Part3/phonebook/backend/mongo.js
@@ -1,32 +1,45 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-if (process.argv.length<3) {
-  console.log('give password as argument')
-  process.exit(1)
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
 }
 
-const password = process.argv[2]
+const password = process.argv[2];
 
-const url = `mongodb+srv://fullstack:${password}@cluster0.dhycoa8.mongodb.net/phonebook?retryWrites=true&w=majority&appName=Cluster0`
+const url = `mongodb+srv://fullstack:${password}@cluster0.dhycoa8.mongodb.net/phonebook?retryWrites=true&w=majority&appName=Cluster0`;
 
-mongoose.set('strictQuery',false)
+mongoose.set("strictQuery", false);
 
-mongoose.connect(url)
+mongoose.connect(url);
 
 const personSchema = new mongoose.Schema({
   name: String,
   number: String,
-})
+});
 
-const Person = mongoose.model('Person', personSchema)
+const Person = mongoose.model("Person", personSchema);
 
-const person = new Person({
-  name: 'FirstName LastName2',
-  number: '000-000-0000',
-})
+// add someone to phonebook
+if (process.argv.length === 5) {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  });
 
-person.save().then(result => {
-  console.log('person saved!')
-  console.log(result)
-  mongoose.connection.close()
-})
+  person.save().then((result) => {
+    console.log(`${process.argv[3]} saved with number ${process.argv[4]}`);
+    mongoose.connection.close();
+  });
+}
+
+// print from database
+if (process.argv.length === 3) {
+  Person.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(`${person.name} ${person.number}`);
+      //console.log(person);
+    });
+    mongoose.connection.close();
+  });
+}
